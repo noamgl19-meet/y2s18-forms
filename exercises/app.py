0757f4f97276a41ab74373696293d80af9bd2536
aboud20-meet <aboud20@meet.mit.edu>
2018-08-08 16:45:06 +0300

    holden

--- exercises/app.py
+++ exercises/app.py
@@ -1,5 +1,6 @@
 from databases import *
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,4 +11,17 @@ def home():
 def display_student(student_id):
     return render_template('student.html', student=query_by_id(student_id))
 
+@app.route('/add', methods = ['GET','POST'])
+def add_student_route():
+	if request.method == 'GET':
+		return render_template(
+			"add.html"
+
+			)
+	else:
+		print("recieved a POST request")
+		return render_template(
+			"add.html"
+
+			)
 app.run(debug=True)
